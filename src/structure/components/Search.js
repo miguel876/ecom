7372f1cb0bb2e6d7b7ea34b08f27f70d47c9f83d
miguel <miguel.santos@pst.asseco.com>
2021-04-15 18:20:20 +0100

    Changes to search loading

--- src/structure/components/Search.js
+++ src/structure/components/Search.js
@@ -16,6 +16,7 @@ export default class Search extends Component {
            productsSearch: [],
            typedSearch : false,
            loaded: false,
+           typing: false,
         };
     }
     
@@ -23,12 +24,12 @@ export default class Search extends Component {
         store.dispatch({type: SHOW_PRODUCTS});
         const getProductState = store.getState();
         
-        this.setState({products: getProductState.products, loaded: true});
+        this.setState({products: getProductState.products});
 
     }
 
     searchProducts = (typed) =>{
-        
+        this.setState({typing: true, loaded: true});
         if(typed){
             //Iterate the products array
             let prdArray = [];
@@ -37,20 +38,22 @@ export default class Search extends Component {
                 let cleanName = prd.name.toLowerCase().replace(/\s/g, '');
                 //Search for name and tags
                 if(cleanName.includes(typed.target.value.toLowerCase())){
-                   //Handle the list, add to state and rerender again, no need for a new a component
+                    //Handle the list, add to state and rerender again
                     if(typed.target.value){
                         prdArray.push(prd);
-                        this.setState({ productsSearch: prdArray });
+                        this.setState({ productsSearch: prdArray, typedSearch: true });
                     }else{
-                        this.setState({typedSearch: false});
+                        this.setState({typedSearch: false, typing: false });
                     }
                 }else{
                     //Show or hide the result box
-                    !prdArray.length ?  this.setState({typedSearch: false}) :  this.setState({typedSearch: true});
+                    !prdArray.length ?  this.setState({typedSearch: false }) :  this.setState({typedSearch: true});
                 }
-
             }
+
         }
+
+        this.setState({ loaded: false });
     }
 
     render() {
@@ -70,34 +73,43 @@ export default class Search extends Component {
                     {this.state.typedSearch}
                         <SearchIcon className="search-icon"/>
                         <input type="text" name="name" placeholder="Search" autoComplete="off" onChange={this.searchProducts}/>
-                        { !this.state.loaded ? 
-                            <LoadingComponents /> : ""
-                        }
-                        { this.state.typedSearch === true ?
-                            <div className="search-box-results">
-                            {
-                                    this.state.productsSearch.map((item, key) => (
-                                        <Link key={key} to={"/product/" + item.id}>
-                                            <div className="search-result-tab">
-                                                <div className="search-prd-left">
-                                                    <div className="search-prd-image"><img src={imageSrc + item.filename} alt="Product" /></div>
-                                                </div>
-                                                <div className="search-prd-right">
-                                                    <div className="search-prd-right-1">
-                                                        <div className="search-prd-name">{item.name}</div>
-                                                        <div className="search-prd-desc">{item.description}</div>
+                        { 
+                            !this.state.loaded ?
+                                this.state.typedSearch ?
+                                <div className="search-box-results">
+                                {
+                                        this.state.productsSearch.map((item, key) => (
+                                            <Link key={key} to={"/product/" + item.id}>
+                                                <div className="search-result-tab">
+                                                    <div className="search-prd-left">
+                                                        <div className="search-prd-image"><img src={imageSrc + item.filename} alt="Product" /></div>
                                                     </div>
-                                                    <div className="search-prd-right-2">
-                                                        <div className="search-prd-price">{item.price + mainCurr}</div>
+                                                    <div className="search-prd-right">
+                                                        <div className="search-prd-right-1">
+                                                            <div className="search-prd-name">{item.name}</div>
+                                                            <div className="search-prd-desc">{item.description}</div>
+                                                        </div>
+                                                        <div className="search-prd-right-2">
+                                                            <div className="search-prd-price">{item.price + mainCurr}</div>
+                                                        </div>
                                                     </div>
+                                                
                                                 </div>
-                                            
-                                            </div>
-                                        </Link>
-                                    ))  
-                            }
-                            </div>
-                        : ""
+                                            </Link>
+                                        )) 
+                                }
+                                </div>
+                            : 
+                            this.state.typing ? 
+                                <div className="search-box-results">
+                                    <div className="search-empty">There are no results matching you search.</div>
+                                </div> 
+                            : ""
+
+                        : 
+                        <div className="search-box-results">
+                            <LoadingComponents />
+                        </div>
                         }
                     </div>
                 </div>
